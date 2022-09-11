966d57852795a166bd653f80d87db29bf631a7f5
Engin Bolat <engi.bolat@gmail.com>
2022-09-11 17:14:38 +0300

    navigate to home or first page

---
+++ lib/view/navigator_page/navigator_page.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/view/take_name_page/take_name_page.dart';
+
+import '../../service/shared/shared_service.dart';
+import '../bottom_nav_bar_page/bottom_nav_bar_page.dart';
+
+class NavigatorPage extends StatefulWidget {
+  const NavigatorPage({Key? key}) : super(key: key);
+
+  @override
+  State<NavigatorPage> createState() => _NavigatorPageState();
+}
+
+class _NavigatorPageState extends State<NavigatorPage> {
+  final SharedService service = SharedService();
+  bool? isLogin;
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    getLogin();
+    super.initState();
+  }
+
+  void getLogin() async {
+    changeIsLoading();
+    Future.delayed(const Duration(seconds: 2));
+    isLogin = await service.getLogin();
+    setState(() => isLogin);
+    changeIsLoading();
+  }
+
+  void changeIsLoading() {
+    setState(() {
+      isLoading = !isLoading;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: isLoading == true
+          ? CircularProgressIndicator(
+              color: Theme.of(context).primaryColor,
+            )
+          : isLogin != true
+              ? TakeNamePage()
+              : const BottomNavBarPage(),
+    );
+  }
+}
