81f97a981592ee295e722175ebae3682ca2a726e
Engin Bolat <engi.bolat@gmail.com>
2022-09-11 17:11:09 +0300

    App Texts

---
+++ lib/constants/app_text.dart
@@ -0,0 +1,15 @@
+class AppText {
+  static String appName = "To-Do";
+  static String welcome = "Hoşgeldin!";
+  static String goodMorning = "Günaydın!";
+  static String goodNight = "Yakşamlar!";
+  static String canYouTypeYourName =
+      "Uygulamada kullanmamız için ismini yazar mısın?";
+  static String yourName = "Adınız...";
+  static String yourSurname = "Soyadınız...";
+  static String letsstart = "Başla!";
+  static String addSometingTodo = "Yapılcak Bir İş Ekle!";
+  static String title = "Başlık";
+  static String desc = "Açıklama";
+  static String selectDate = "Tarih Seç";
+}
