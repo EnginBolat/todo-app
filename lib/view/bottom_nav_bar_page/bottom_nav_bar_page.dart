06637e51c96a61cea7a6b5e4f8205d125c9457c3
Engin Bolat <engi.bolat@gmail.com>
2022-09-12 20:59:36 +0300

    Add Todo Page Navigation
    lear

--- lib/view/bottom_nav_bar_page/bottom_nav_bar_page.dart
+++ lib/view/bottom_nav_bar_page/bottom_nav_bar_page.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:todo_app/view/bottom_nav_bar_page/add_todo_page.dart';
 import 'package:todo_app/view/celendar_page/calendar_page.dart';
 import 'package:todo_app/view/done_page/done_page.dart';
 import 'package:todo_app/view/home_page/home_page.dart';
 import 'package:todo_app/view/profile_page/profile_page.dart';
 import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
-
-import '../../constants/app_text.dart';
 
 class BottomNavBarPage extends StatefulWidget {
   const BottomNavBarPage({Key? key}) : super(key: key);
@@ -18,6 +17,8 @@ class BottomNavBarPage extends StatefulWidget {
 class _BottomNavBarPageState extends State<BottomNavBarPage> {
   final TextEditingController titleController = TextEditingController();
   final TextEditingController descController = TextEditingController();
+  var _selectedDate;
+
   final List<Widget> pages = const [
     HomePage(),
     CalendarPage(),
@@ -33,7 +34,6 @@ class _BottomNavBarPageState extends State<BottomNavBarPage> {
   ];
 
   int currentIndex = 0;
-  DateTime? selectedDate;
   Widget currentPage = const HomePage();
 
   @override
@@ -42,16 +42,16 @@ class _BottomNavBarPageState extends State<BottomNavBarPage> {
       body: currentPage,
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          _buildBottomSheet(context, titleController, descController);
+          _buildBottomSheet(context);
         },
         child: const Icon(Icons.add),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      bottomNavigationBar: _bottomNavBar(context),
+      bottomNavigationBar: _bottomNavBar(),
     );
   }
 
-  AnimatedBottomNavigationBar _bottomNavBar(BuildContext context) {
+  AnimatedBottomNavigationBar _bottomNavBar() {
     return AnimatedBottomNavigationBar(
       icons: icons,
       activeIndex: currentIndex,
@@ -66,191 +66,12 @@ class _BottomNavBarPageState extends State<BottomNavBarPage> {
     );
   }
 
-  Future<dynamic> _buildBottomSheet(BuildContext context,
-      TextEditingController titleController, TextEditingController desc) {
+  Future<dynamic> _buildBottomSheet(BuildContext ctx) {
     return showModalBottomSheet(
-      context: context,
-      builder: (BuildContext context) {
-        return Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Column(
-                children: [
-                  Text(
-                    AppText.addSometingTodo,
-                    style: Theme.of(context).textTheme.headline5!.copyWith(
-                          color: Colors.grey,
-                          fontWeight: FontWeight.w800,
-                        ),
-                  ),
-                  _buildSpace(20.0),
-                  AddTodoTextField(
-                    hintText: AppText.title,
-                    controller: titleController,
-                  ),
-                  _buildSpace(12.0),
-                  AddTodoDescTextField(
-                    hintText: AppText.desc,
-                    controller: descController,
-                  ),
-                  _buildSpace(12.0),
-                ],
-              ),
-              SizedBox(
-                height: 70,
-                child: Row(
-                  // crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Expanded(
-                      child: Text(
-                        selectedDate == null
-                            ? 'Tarih Seçilmedi!'
-                            : 'Seçilen Tarih: ${DateFormat.yMMMMd().format(selectedDate!)}',
-                      ),
-                    ),
-                    TextButton(
-                      onPressed: () => presentDatePicker(context),
-                      child: Text(AppText.selectDate),
-                    ),
-                  ],
-                ),
-              ),
-              _buildAddTodoButton(),
-            ],
-          ),
-        );
+      context: ctx,
+      builder: (bctx) {
+        return AddTodoPage();
       },
-    );
-  }
-
-  void presentDatePicker(context) {
-    showDatePicker(
-      context: context,
-      initialDate: DateTime.now(),
-      firstDate: DateTime.now(),
-      lastDate: DateTime(2025),
-    ).then((pickedDate) {
-      if (pickedDate == null) {
-        return;
-      } else {
-        setState(() {
-          selectedDate = pickedDate;
-        });
-      }
-    });
-  }
-
-  ClipRRect _buildAddTodoButton() {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(12),
-      child: SizedBox(
-        width: double.infinity,
-        height: 40,
-        child: ElevatedButton(
-          onPressed: () {
-            if (titleController.text.isNotEmpty &&
-                descController.text.isNotEmpty) {
-              print("Başlık: ${titleController.text}");
-              print("Açıklama: ${descController.text}");
-              descController.clear();
-              titleController.clear();
-              Navigator.pop(context);
-            }
-          },
-          child: const Text("Ekle"),
-        ),
-      ),
-    );
-  }
-}
-
-SizedBox _buildSpace(double height) => SizedBox(height: height);
-
-class AddTodoTextField extends StatelessWidget {
-  const AddTodoTextField({
-    Key? key,
-    required this.hintText,
-    required this.controller,
-  }) : super(key: key);
-
-  final String hintText;
-  final TextEditingController controller;
-
-  @override
-  Widget build(BuildContext context) {
-    return TextField(
-      maxLength: 25,
-      controller: controller,
-      decoration: InputDecoration(
-        hintText: hintText,
-        hoverColor: Theme.of(context).primaryColor,
-        enabledBorder: const OutlineInputBorder(
-          borderSide: BorderSide(
-            color: Colors.grey,
-            width: 2.0,
-          ),
-        ),
-        focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide(
-            color: Theme.of(context).primaryColor,
-            width: 2.0,
-          ),
-        ),
-        errorBorder: const OutlineInputBorder(
-          borderSide: BorderSide(
-            color: Colors.red,
-            width: 2.0,
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class AddTodoDescTextField extends StatelessWidget {
-  const AddTodoDescTextField({
-    Key? key,
-    required this.hintText,
-    required this.controller,
-  }) : super(key: key);
-
-  final String hintText;
-  final TextEditingController controller;
-
-  @override
-  Widget build(BuildContext context) {
-    return TextField(
-      maxLength: 120,
-      maxLines: 2,
-      controller: controller,
-      decoration: InputDecoration(
-        isDense: true,
-        contentPadding:
-            const EdgeInsets.symmetric(vertical: 24.0, horizontal: 8.0),
-        hintText: hintText,
-        hoverColor: Theme.of(context).primaryColor,
-        enabledBorder: const OutlineInputBorder(
-          borderSide: BorderSide(
-            color: Colors.grey,
-            width: 2.0,
-          ),
-        ),
-        focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide(
-            color: Theme.of(context).primaryColor,
-            width: 2.0,
-          ),
-        ),
-        errorBorder: const OutlineInputBorder(
-          borderSide: BorderSide(
-            color: Colors.red,
-            width: 2.0,
-          ),
-        ),
-      ),
     );
   }
 }
