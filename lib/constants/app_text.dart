3bd1f23606bd9a63e466152f2ff290efcdfe2d3b
Engin Bolat <engi.bolat@gmail.com>
2022-09-14 23:18:43 +0300

    Seperated Text page by page

--- lib/constants/app_text.dart
+++ lib/constants/app_text.dart
@@ -1,19 +1,45 @@
 class AppText {
   static String appName = "To-Do";
+}
+
+class IntroPageText {
   static String welcome = "Hoşgeldin!";
-  static String goodMorning = "Günaydın!";
-  static String goodNight = "Yakşamlar!";
+
   static String canYouTypeYourName =
       "Uygulamada kullanmamız için ismini yazar mısın?";
   static String yourName = "Adınız...";
   static String yourSurname = "Soyadınız...";
   static String letsstart = "Başla!";
+}
+
+class HomePageText {
+  static String welcome = "Hoşgeldin!";
+  static String goodMorning = "Günaydın!";
+  static String goodNight = "Yakşamlar!";
   static String addSometingTodo = "Yapılcak Bir İş Ekle!";
-  static String title = "Başlık";
-  static String desc = "Açıklama (İsteğe Bağlı)";
+  static String waitingTodos = "Bekleyen iş sayınız:";
+  static String todos = "İşleriniz";
+
+
+}
+
+class AddTodoPageText {
+  static String addSometingTodo = "Yapılcak Bir İş Ekle!";
+  static String title = "Başlık...";
+  static String desc = "Açıklama... (İsteğe Bağlı)";
   static String selectDate = "Tarih Seç";
   static String dateCannotBeEmpty = "Tarih Boş Geçilemez!";
   static String titleCannotBeEmpty = "Başlık Boş Geçilemez!";
   static String chosendate = "Seçilen Tarih";
   static String add = "Ekle";
+}
+
+class CalendarPageText {}
+
+class DonePageText {}
+
+class ProfileTodoPage {}
+
+class BlocErrorText{
+  static String  dataCantFetch= "Veri Çekilemedi";
 }
