1d01258bc0aa3b6b3707e8866822892c15602ab2
Engin Bolat <engi.bolat@gmail.com>
2022-09-18 11:48:29 +0300

    Celandar Added

--- lib/view/calendar_page/calendar_page.dart
+++ lib/view/calendar_page/calendar_page.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+import '../../constants/app_padding.dart';
 
 class CalendarPage extends StatelessWidget {
   const CalendarPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("CalendarPage")),
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.only(top:AppPadding.maxValue),
+        child: Column(
+          children: [
+            TableCalendar(
+              firstDay: DateTime.utc(2010, 10, 16),
+              lastDay: DateTime.utc(2030, 3, 14),
+              focusedDay: DateTime.now(),
+            ),
+            Center(
+              child: Text("CalendarPage"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
