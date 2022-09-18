552f76b9db61afc280ca2b7f0bace8a52877ade0
Engin Bolat <engi.bolat@gmail.com>
2022-09-18 11:51:24 +0300

    Update name and surname added

--- lib/view/profile_page/profile_page.dart
+++ lib/view/profile_page/profile_page.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/constants/app_padding.dart';
+import 'package:todo_app/model/todo_model.dart';
 import 'package:todo_app/service/shared/shared_service.dart';
+
+import '../../constants/app_radius.dart';
+import '../../constants/app_text.dart';
+import '../../service/db/database_service.dart';
+import '../bottom_nav_bar_page/bottom_nav_bar_page.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key}) : super(key: key);
@@ -13,11 +20,17 @@ class _ProfilePageState extends State<ProfilePage> {
   String? name;
   String? surname;
   bool isLoading = false;
+  late List<Todo> _todoDoneList;
+  late List<Todo> _todoList;
+  TextEditingController nameController = TextEditingController();
+  TextEditingController surnameController = TextEditingController();
 
   void getDatas() async {
     changeIsLoading();
     name = await service.getName();
     surname = await service.getSurname();
+    _todoList = await DatabaseService().getAllNotes();
+    _todoDoneList = await DatabaseService().getDoneTodos();
     Future.delayed(const Duration(seconds: 2));
     changeIsLoading();
   }
@@ -34,41 +47,297 @@ class _ProfilePageState extends State<ProfilePage> {
     getDatas();
   }
 
+  void updateNames() {
+    service.setName(nameController.text);
+    service.setSurname(surnameController.text);
+  }
+
   @override
   Widget build(BuildContext context) {
+    final double deviceHeight = MediaQuery.of(context).size.height;
+    final double deviceWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       body: isLoading == true
           ? CircularProgressIndicator(color: Theme.of(context).primaryColor)
           : Padding(
-              padding: const EdgeInsets.all(8.0),
+              padding: EdgeInsets.all(AppPadding.minValue),
               child: Center(
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
+                    _buildProfilePhoto(deviceHeight, deviceWidth, context),
+                    SpacerWidget(
+                      deviceHeight: deviceHeight,
+                      coefficient: 0.02,
+                    ),
+                    _buidName(context),
+                    SpacerWidget(
+                      deviceHeight: deviceHeight,
+                      coefficient: 0.05,
+                    ),
                     Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
-                        Text(
-                          "$name $surname".toUpperCase(),
-                          style:
-                              Theme.of(context).textTheme.headline4!.copyWith(
-                                    color: Colors.black,
-                                    fontWeight: FontWeight.w500,
-                                  ),
-                        )
+                        BuildInfoBox(
+                          title: ProfilePageText.needTodo,
+                          deviceHeight: deviceHeight,
+                          deviceWidth: deviceWidth,
+                          coefficientHeight: 0.15,
+                          coefficientWidth: 0.4,
+                          list: _todoList,
+                        ),
+                        BuildInfoBox(
+                          title: ProfilePageText.doneTodo,
+                          deviceHeight: deviceHeight,
+                          deviceWidth: deviceWidth,
+                          coefficientHeight: 0.15,
+                          coefficientWidth: 0.4,
+                          list: _todoDoneList,
+                        ),
                       ],
                     ),
-                    ElevatedButton(
-                      onPressed: () async {
-                        service.deleteUserInfo();
-                      },
-                      child: const Text("Dataları Sil"),
-                    )
+                    SpacerWidget(
+                      deviceHeight: deviceHeight,
+                      coefficient: 0.02,
+                    ),
+                    BuildAllInfoBox(
+                      title: ProfilePageText.allTodo,
+                      deviceHeight: deviceHeight,
+                      deviceWidth: deviceWidth,
+                      coefficientHeight: 0.15,
+                      coefficientWidth: 0.85,
+                      counter: (_todoList.length + _todoDoneList.length),
+                    ),
                   ],
                 ),
               ),
             ),
+    );
+  }
+
+  Column _buidName(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          "$name $surname".toUpperCase(),
+          style: Theme.of(context).textTheme.headline4!.copyWith(
+                color: Colors.black,
+                fontWeight: FontWeight.w500,
+              ),
+        ),
+        IconButton(
+          onPressed: () {
+            _buildUpdateName(context);
+          },
+          icon: const Icon(
+            Icons.edit,
+          ),
+        )
+      ],
+    );
+  }
+
+  Future<dynamic> _buildUpdateName(BuildContext context) {
+    return showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return Padding(
+          padding: EdgeInsets.all(AppPadding.normalValue),
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height * 0.6,
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.9,
+                    child: CustomTextField(
+                      controller: nameController,
+                      hintText: ProfilePageText.yourName,
+                    ),
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.9,
+                    child: CustomTextField(
+                      controller: surnameController,
+                      hintText: ProfilePageText.yourSurname,
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      updateNames();
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const BottomNavBarPage(
+                                  index: 3,
+                                )),
+                      );
+                    },
+                    child: Text(ProfilePageText.updateName),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  ClipRRect _buildProfilePhoto(
+      double deviceHeight, double deviceWidth, BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(AppRadius.profileRadius),
+      child: Container(
+        height: deviceHeight * 0.2,
+        width: deviceWidth * 0.45,
+        color: Theme.of(context).primaryColor,
+      ),
+    );
+  }
+}
+
+class SpacerWidget extends StatelessWidget {
+  const SpacerWidget(
+      {Key? key, required this.deviceHeight, required this.coefficient})
+      : super(key: key);
+
+  final double deviceHeight;
+  final double coefficient;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: deviceHeight * coefficient,
+    );
+  }
+}
+
+class BuildInfoBox extends StatelessWidget {
+  const BuildInfoBox({
+    Key? key,
+    required this.deviceHeight,
+    required this.deviceWidth,
+    required this.list,
+    required this.title,
+    required this.coefficientHeight,
+    required this.coefficientWidth,
+  }) : super(key: key);
+
+  final double deviceHeight;
+  final double deviceWidth;
+  final double coefficientHeight;
+  final double coefficientWidth;
+  final String title;
+  final List<Todo> list;
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: Container(
+        color: Theme.of(context).primaryColor,
+        height: deviceHeight * coefficientHeight,
+        width: deviceWidth * coefficientWidth,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              title,
+              style: Theme.of(context).textTheme.headline6!.copyWith(
+                    color: Colors.white,
+                  ),
+            ),
+            SpacerWidget(
+              deviceHeight: deviceHeight,
+              coefficient: 0.01,
+            ),
+            Text(
+              list.length.toString(),
+              style: Theme.of(context).textTheme.headline6!.copyWith(
+                    color: Colors.white,
+                  ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BuildAllInfoBox extends StatelessWidget {
+  const BuildAllInfoBox({
+    Key? key,
+    required this.deviceHeight,
+    required this.deviceWidth,
+    required this.counter,
+    required this.title,
+    required this.coefficientHeight,
+    required this.coefficientWidth,
+  }) : super(key: key);
+
+  final double deviceHeight;
+  final double deviceWidth;
+  final double coefficientHeight;
+  final double coefficientWidth;
+  final String title;
+  final int counter;
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: Container(
+        color: Theme.of(context).primaryColor,
+        height: deviceHeight * coefficientHeight,
+        width: deviceWidth * coefficientWidth,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              title,
+              style: Theme.of(context).textTheme.headline6!.copyWith(
+                    color: Colors.white,
+                  ),
+            ),
+            SpacerWidget(
+              deviceHeight: deviceHeight,
+              coefficient: 0.01,
+            ),
+            Text(
+              counter.toString(),
+              style: Theme.of(context).textTheme.headline6!.copyWith(
+                    color: Colors.white,
+                  ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({
+    Key? key,
+    required this.controller,
+    required this.hintText,
+  }) : super(key: key);
+
+  final TextEditingController controller;
+  final String hintText;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      maxLength: 15,
+      decoration: InputDecoration(
+        hintText: hintText,
+      ),
+      controller: controller,
     );
   }
 }
