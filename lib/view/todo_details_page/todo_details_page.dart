c1b9bc69a3495fe0067799b0e9735d7fd10cf87d
Engin Bolat <engi.bolat@gmail.com>
2022-10-07 20:51:26 +0300

    Navigate to editToDoPage

--- lib/view/todo_details_page/todo_details_page.dart
+++ lib/view/todo_details_page/todo_details_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todo_app/constants/app_padding.dart';
 import 'package:todo_app/constants/app_radius.dart';
 import 'package:todo_app/service/cubit/todo_cubit.dart';
+import 'package:todo_app/view/edit_todo_page/edit_todo_page.dart';
 import 'package:todo_app/widgets/spacer_widget.dart';
 import 'package:todo_app/widgets/text_date_format.dart';
 
@@ -28,25 +29,33 @@ class TodoDetailsPage extends StatelessWidget {
             Padding(
               padding: EdgeInsets.all(AppPadding.minValue),
               child: IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  _buildBottomSheet(context,id);
+                  // Navigator.push(
+                  //     context,
+                  //     MaterialPageRoute(
+                  //         builder: (context) => EditTodoPage(id: id)));
+                },
                 icon: const Icon(Icons.edit),
                 color: Colors.black,
               ),
             ),
           ],
         ),
-        body: BlocConsumer<TodoCubit, TodoState>(
-          listener: (context, state) {},
-          builder: (context, state) {
-            if (state is GetTodoById) {
-              Todo? item = state.todoItem;
-              return _buildTodoDetails(item, context);
-            } else if (state is TodoGetDataError) {
-              return _buildErrorWidget(state);
-            } else {
-              return const Center(child: CircularProgressIndicator());
-            }
-          },
+        body: SingleChildScrollView(
+          child: BlocConsumer<TodoCubit, TodoState>(
+            listener: (context, state) {},
+            builder: (context, state) {
+              if (state is GetTodoById) {
+                Todo? item = state.todoItem;
+                return _buildTodoDetails(item, context);
+              } else if (state is TodoGetDataError) {
+                return _buildErrorWidget(state);
+              } else {
+                return const Center(child: CircularProgressIndicator());
+              }
+            },
+          ),
         ),
       ),
     );
@@ -54,105 +63,102 @@ class TodoDetailsPage extends StatelessWidget {
 
   Widget _buildTodoDetails(Todo? item, BuildContext context) {
     double deviceHeight = MediaQuery.of(context).size.height;
-    return SingleChildScrollView(
-      child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: AppPadding.normalValue),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              FittedBox(
-                child: Text(
-                  item?.title.toUpperCase() ?? "",
-                  style: Theme.of(context)
-                      .textTheme
-                      .headline5!
-                      .copyWith(fontWeight: FontWeight.w600),
-                  textAlign: TextAlign.center,
-                ),
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: AppPadding.normalValue),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FittedBox(
+              child: Text(
+                item?.title.toUpperCase() ?? "",
+                style: Theme.of(context)
+                    .textTheme
+                    .headline5!
+                    .copyWith(fontWeight: FontWeight.w600),
+                textAlign: TextAlign.center,
               ),
-              SpacerWidget(
-                deviceHeight: MediaQuery.of(context).size.height,
-                coefficient: 0.05,
+            ),
+            SpacerWidget(
+              deviceHeight: MediaQuery.of(context).size.height,
+              coefficient: 0.05,
+            ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(
+                AppRadius.maxValue,
               ),
-              ClipRRect(
-                borderRadius: BorderRadius.circular(
-                  AppRadius.maxValue,
-                ),
-                child: Container(
-                  height: deviceHeight * 0.3,
-                  color: Theme.of(context).primaryColor,
-                  child: Padding(
-                    padding: EdgeInsets.all(AppPadding.maxValue),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "AÇIKLAMA",
-                          style:
-                              Theme.of(context).textTheme.headline5!.copyWith(
-                                    fontWeight: FontWeight.w500,
+              child: Container(
+                height: deviceHeight * 0.3,
+                color: Theme.of(context).primaryColor,
+                child: Padding(
+                  padding: EdgeInsets.all(AppPadding.maxValue),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "AÇIKLAMA",
+                        style: Theme.of(context).textTheme.headline5!.copyWith(
+                              fontWeight: FontWeight.w500,
+                              color: Colors.white,
+                            ),
+                      ),
+                      SpacerWidget(
+                        deviceHeight: deviceHeight,
+                        coefficient: 0.02,
+                      ),
+                      item!.description.isEmpty
+                          ? Text(
+                              "Açıklama Bulunamadı",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .subtitle1!
+                                  .copyWith(
                                     color: Colors.white,
                                   ),
-                        ),
-                        SpacerWidget(
-                          deviceHeight: deviceHeight,
-                          coefficient: 0.02,
-                        ),
-                        item!.description.isEmpty
-                            ? Text(
-                                "Açıklama Bulunamadı",
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .subtitle1!
-                                    .copyWith(
-                                      color: Colors.white,
-                                    ),
-                              )
-                            : Text(
-                                item.description,
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .subtitle1!
-                                    .copyWith(
-                                      color: Colors.white,
-                                    ),
-                              ),
-                        SpacerWidget(
-                          deviceHeight: deviceHeight,
-                          coefficient: 0.1,
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            item.isDone
-                                ? const Text(
-                                    "Tamamlandı",
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                    ),
-                                  )
-                                : const Text(
-                                    "Tamamlanmadı",
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                    ),
+                            )
+                          : Text(
+                              item.description,
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .subtitle1!
+                                  .copyWith(
+                                    color: Colors.white,
                                   ),
-                            Align(
-                              alignment: Alignment.centerRight,
-                              child: TextDateFormat(
-                                date: item.createdDate,
-                              ),
                             ),
-                          ],
-                        ),
-                      ],
-                    ),
+                      SpacerWidget(
+                        deviceHeight: deviceHeight,
+                        coefficient: 0.1,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          item.isDone
+                              ? const Text(
+                                  "Tamamlandı",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                  ),
+                                )
+                              : const Text(
+                                  "Tamamlanmadı",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                          Align(
+                            alignment: Alignment.centerRight,
+                            child: TextDateFormat(
+                              date: item.createdDate,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
                 ),
-              )
-            ],
-          ),
+              ),
+            )
+          ],
         ),
       ),
     );
@@ -160,4 +166,13 @@ class TodoDetailsPage extends StatelessWidget {
 
   Center _buildErrorWidget(TodoGetDataError state) =>
       Center(child: Text(state.errorMessage));
+}
+
+Future<dynamic> _buildBottomSheet(BuildContext ctx,int id) {
+  return showModalBottomSheet(
+    context: ctx,
+    builder: (bctx) {
+      return EditTodoPage(id:id);
+    },
+  );
 }
