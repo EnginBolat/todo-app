cf195954c20b113a9adc0c6abe671d0846fa29bb
Engin Bolat <engi.bolat@gmail.com>
2022-10-07 22:11:56 +0300

    UI Done

--- lib/view/edit_todo_page/edit_todo_page.dart
+++ lib/view/edit_todo_page/edit_todo_page.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todo_app/service/db/database_service.dart';
+import 'package:todo_app/view/home_page/home_page.dart';
 import 'package:todo_app/widgets/spacer_widget.dart';
-import 'package:todo_app/widgets/text_date_format.dart';
 
 import '../../constants/app_padding.dart';
 import '../../model/todo_model.dart';
-import '../../service/cubit/todo_cubit.dart';
+import '../../widgets/text_date_format.dart';
 
 class EditTodoPage extends StatefulWidget {
   EditTodoPage({Key? key, required this.id}) : super(key: key);
@@ -18,19 +17,30 @@ class EditTodoPage extends StatefulWidget {
 
 class _EditTodoPageState extends State<EditTodoPage> {
   final TextEditingController titleController = TextEditingController();
-
   final TextEditingController descController = TextEditingController();
+  Todo? item;
+  DateTime? currentDate;
+  @override
+  void initState() {
+    super.initState();
+    fetchDataById(widget.id);
+  }
+
+  fetchDataById(int id) async {
+    item = await DatabaseService().getTodoById(id);
+    titleController.text = item!.title;
+    descController.text = item!.description;
+    print(item!.createdDate);
+  }
 
   @override
   Widget build(BuildContext context) {
-    late DateTime currentDate;
-    late DateTime createdDate;
-    late int createdHour;
+    final double deviceHeight = MediaQuery.of(context).size.height;
 
     void _presentDatePicker() {
       showDatePicker(
         context: context,
-        initialDate: currentDate,
+        initialDate: DateTime.now(),
         firstDate: DateTime(2022),
         lastDate: DateTime(2025),
       ).then((pickedDate) {
@@ -39,86 +49,89 @@ class _EditTodoPageState extends State<EditTodoPage> {
         }
         setState(() {
           currentDate = pickedDate;
-          createdDate = currentDate;
-          createdHour = pickedDate.hour;
+          print(currentDate);
         });
       });
     }
 
-    return BlocProvider(
-      create: (context) => TodoCubit()..getTodoById(widget.id),
-      child: Scaffold(
-        appBar: AppBar(
-          toolbarHeight: MediaQuery.of(context).size.height * 0.1,
-          leading: IconButton(
-            onPressed: () => Navigator.pop(context),
-            icon: const Icon(Icons.chevron_left),
-            color: Colors.black,
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: deviceHeight * 0.1,
+        leading: IconButton(
+          onPressed: () => Navigator.pop(context),
+          icon: const Icon(Icons.chevron_left),
+          color: Colors.black,
         ),
-        body: SingleChildScrollView(
-          child: BlocConsumer<TodoCubit, TodoState>(
-            listener: (context, state) {},
-            builder: (context, state) {
-              if (state is GetTodoById) {
-                Todo? item = state.todoItem;
-                currentDate = item!.createdDate;
-                titleController.text = item.title;
-                descController.text = item.description;
-                double deviceHeight = MediaQuery.of(context).size.height;
-                return Padding(
-                  padding: EdgeInsets.all(AppPadding.normalValue),
-                  child: Column(
-                    children: [
-                      TextField(
-                        controller: titleController,
-                        maxLength: 25,
-                        decoration: const InputDecoration(
-                          label: Text("Başlık"),
-                        ),
-                      ),
-                      SpacerWidget(
-                          deviceHeight: deviceHeight, coefficient: 0.02),
-                      TextField(
-                        controller: descController,
-                        maxLines: 5,
-                        maxLength: 120,
-                        decoration: const InputDecoration(
-                          label: Text("Açıklama"),
-                        ),
-                      ),
-                      SpacerWidget(
-                          deviceHeight: deviceHeight, coefficient: 0.01),
-                      Align(
-                        alignment: Alignment.centerRight,
-                        child: TextButton(
-                          onPressed: _presentDatePicker,
-                          child: TextDateFormat(
-                            date: currentDate,
-                            textColor: Theme.of(context).primaryColor,
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        width: double.infinity,
-                        height: deviceHeight * 0.05,
-                        child: ElevatedButton(
-                          onPressed: () {},
-                          child: const Text(
-                            "Güncelle",
-                          ),
-                        ),
-                      )
-                    ],
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(AppPadding.normalValue),
+          child: Column(
+            children: [
+              TextField(
+                controller: titleController,
+                maxLength: 25,
+                decoration: const InputDecoration(
+                  label: Text("Başlık"),
+                ),
+              ),
+              SpacerWidget(deviceHeight: deviceHeight, coefficient: 0.02),
+              TextField(
+                controller: descController,
+                maxLines: 5,
+                maxLength: 120,
+                decoration: const InputDecoration(
+                  label: Text("Açıklama"),
+                ),
+              ),
+              SpacerWidget(deviceHeight: deviceHeight, coefficient: 0.01),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: TextButton(
+                      onPressed: _presentDatePicker,
+                      child: const Text("Tarih Seçiniz"),
+                    ),
                   ),
-                );
-              } else {
-                return const Center(child: CircularProgressIndicator());
-              }
-            },
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: TextDateFormat(
+                      date: currentDate ?? DateTime.now(),
+                      textColor: Theme.of(context).primaryColor,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                width: double.infinity,
+                height: deviceHeight * 0.05,
+                child: ElevatedButton(
+                  onPressed: () async {
+                    updateTodo();
+                    Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const HomePage()));
+                  },
+                  child: const Text(
+                    "Güncelle",
+                  ),
+                ),
+              )
+            ],
           ),
         ),
       ),
+    );
+  }
+
+  void updateTodo() async {
+    await DatabaseService().updateTodo(
+      item!.title,
+      item!.description,
+      currentDate ?? item!.createdDate,
     );
   }
 }
